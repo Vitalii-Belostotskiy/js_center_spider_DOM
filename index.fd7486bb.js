51f0be4bca85db875a02987160d01ef6de125f80
Vitalii Bilostotskyi <belostotskiyvitalii84@gmail.com>
2024-09-04 16:11:39 +0300

    make build

---
+++ index.fd7486bb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,l=e.clientHeight/2,o=t.offsetWidth/2,i=t.offsetHeight/2;t.style.left="".concat(c-o,"px"),t.style.top="".concat(l-i,"px");
+//# sourceMappingURL=index.fd7486bb.js.map
